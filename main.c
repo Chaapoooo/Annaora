cc20eacb66b1d8f5713b050c734b2c701167f5a5
leo <grosson.leo@gmail.com>
2026-08-29 09:08:03 +0200

    test: argc & argv

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <dirent.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
     printf("Annaora file manager!\n");
+    printf("%s\n%s\n", argv[0], argv[1]);
     
     return 0;
 }
