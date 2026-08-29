9829801bf494d38217a25cd87a10d6371a285cd0
leo <grosson.leo@gmail.com>
2026-08-29 09:14:00 +0200

    test: using getcwd()

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-#include <dirent.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 30
 
 int main(int argc, char *argv[]) {
 
     printf("Annaora file manager!\n");
     printf("%s\n%s\n", argv[0], argv[1]);
+
+    char buffer[BUFFER_SIZE];
+    if(getcwd(buffer, BUFFER_SIZE) == NULL){
+        printf("Cannot get current working directory path!\n");
+        return 1;
+    }
+
+    printf("Current working directory: %s\n", buffer);
     
     return 0;
 }
