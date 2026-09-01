7db6c56e789ee5eb9b3f7ebd1d47e139954f913a
leo <grosson.leo@gmail.com>
2026-09-02 00:29:29 +0200

    feat: see if files or folder

--- main.c
+++ main.c
@@ -7,6 +7,11 @@
 
 #define BUFFER_SIZE 30
 
+typedef struct{
+    char *name;
+    unsigned char type;
+} File;
+
 int main() {
 
     // TITLE
@@ -25,7 +30,7 @@ int main() {
 
     int capacity = 10;
     int number = 0;
-    char **files = malloc(capacity * sizeof(char *));
+    File *files = malloc(capacity * sizeof(*files));
     if (files == NULL) {
         perror("malloc");
         return 1;
@@ -33,13 +38,21 @@ int main() {
 
     struct dirent *entry;
     while((entry = readdir(dir)) != NULL){
+
         if(number >= capacity){
             capacity *= 2;
-            files = realloc(files, capacity * sizeof(char *));
+            files = realloc(files, capacity * sizeof(*files));
         }
-        files[number] = malloc(strlen(entry->d_name) +1);
-        strcpy(files[number], entry->d_name);
-        printf("%s\n", files[number]);
+
+        files[number].name = malloc(strlen(entry->d_name) + 1);
+        strcpy(files[number].name, entry->d_name);
+        files[number].type = entry->d_type;
+
+        if(files[number].type == 4){
+            printf("FOLDER - %s\n", files[number].name);
+        } else if (files[number].type == 8){
+            printf("FILE   - %s\n", files[number].name);
+        }
         number++;
     }
 
